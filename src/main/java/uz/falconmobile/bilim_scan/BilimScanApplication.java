a98d727360b5c2f7cc2779a781d5fc3e857eecd1
yengilmas <lbek458@gmail.com>
2026-09-08 11:17:56 +0500

    boshlangiz configuratsiyalar qilindi

---
+++ src/main/java/uz/falconmobile/bilim_scan/BilimScanApplication.java
@@ -0,0 +1,13 @@
+package uz.falconmobile.bilim_scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilimScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilimScanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/falconmobile/bilim_scan/BilimScanApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.falconmobile.bilim_scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilimScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
